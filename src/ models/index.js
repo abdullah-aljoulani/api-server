da5f11ca1eeada54f25433ab8d5330c2b71604d5
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-06-20 16:34:35 +0000

    fixing the error

--- src/ models/index.js
+++ src/ models/index.js
@@ -4,16 +4,26 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const DB_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' :  process.env.DB_URI;
 
-let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  } : {}
+let sequelizeOptions =
+    process.env.NODE_ENV === "production"
+        ? {
+              dialectOptions: {
+                  ssl: false,
+              },
+          }
+        : {};
 
-  let sequelize = new Sequelize(DB_URL, sequelizeOptions);
+// let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     }
+//   } : {}
+
+let sequelize = new Sequelize(process.env.DB_URL, sequelizeOptions);
+
   const clothes = require('./clothes')
   const food = require('./food')
 
